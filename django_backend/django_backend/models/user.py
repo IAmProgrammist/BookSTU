5f867212604dce785a41a46670a2f0d44589ff86
Vlad <vladislav.pakhomov74@gmail.com>
2024-12-23 13:26:24 +0300

    Added indices, fixed register

--- django_backend/django_backend/models/user.py
+++ django_backend/django_backend/models/user.py
@@ -5,8 +5,8 @@ import django_backend.validators.user as user_validators
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     phone_number = models.TextField(blank=False, null=False)
-    surname = models.TextField(blank=False, null=False)
-    name = models.TextField(blank=False, null=False)
-    patronymics = models.TextField(blank=True, null=False)
-    passport_data = models.TextField(blank=False, null=False)
-    banned = models.BooleanField(null=False, default=False)
+    surname = models.TextField(blank=False, null=False, db_index=True)
+    name = models.TextField(blank=False, null=False, db_index=True)
+    patronymics = models.TextField(blank=True, null=False, db_index=True)
+    passport_data = models.TextField(blank=False, null=False, db_index=True)
+    banned = models.BooleanField(null=False, default=False, db_index=True)
