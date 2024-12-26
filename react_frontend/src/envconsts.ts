ee0a4d7437b7c74c1bd6b258d542a83801a2bba8
Vlad <vladislav.pakhomov74@gmail.com>
2024-12-26 22:35:03 +0300

    Added sentry for frontend

--- react_frontend/src/envconsts.ts
+++ react_frontend/src/envconsts.ts
@@ -1,2 +1,3 @@
 export const DEBUG = process.env.REACT_APP_DEBUG === "true";
 export const ENV_API_SERVER = DEBUG ? process.env.REACT_APP_ENV_API_SERVER : window.location.origin; 
+export const SENTRY_DSN = "https://29e5d2d767b9444cbd86563a21e18269@app.glitchtip.com/9662"
